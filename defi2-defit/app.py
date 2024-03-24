76e2e431975735b40e82ad11c78ebf5773ef7ead
ba ablaye <21040@supnum.mr>
2024-03-24 04:18:50 -0700

    good

--- defi2-defit/app.py
+++ defi2-defit/app.py
@@ -1,5 +1,8 @@
+import base64
+import io
+import json
 import webbrowser
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, Response, jsonify, render_template, request
 import folium
 import numpy as np
 import random
@@ -41,6 +44,33 @@ def deposit_pheromone(G, tour, Q):
         u = tour[i]
         v = tour[i + 1]
         G[u][v]['pheromone'] += Q / len(tour)
+        
+
+# uploader les fichier excel
+def upload():
+    capitales_df = pd.read_excel('Cordonnees_GPS.xlsx', sheet_name='Capitales_Wilaya')
+    # mougataas_df = pd.read_excel('Cordonnees_GPS.xlsx', sheet_name='Mougataa')
+
+    G = nx.Graph()
+    
+    for _, row in capitales_df.iterrows():
+        ville, latitude, longitude = row
+        G.add_node(ville, pos=(latitude, longitude))
+
+    # for _, row in mougataas_df.iterrows():
+    #     ville, wilaya, latitude, longitude = row
+    #     G.add_node(ville, pos=(latitude, longitude))
+
+    for u in G.nodes():
+        for v in G.nodes():
+            if u != v and not G.has_edge(u, v):
+                coords_u = G.nodes[u]['pos']
+                coords_v = G.nodes[v]['pos']
+                distance = geopy.distance.geodesic(coords_u, coords_v).km      
+                G.add_edge(u, v, weight=distance)
+    return G
+
+
 
 # Algorithme de colonie de fourmis
 def ant_colony_optimization(G, num_ants, alpha, beta, evaporation_rate, num_iterations, Q):
@@ -73,7 +103,62 @@ def ant_colony_optimization(G, num_ants, alpha, beta, evaporation_rate, num_iter
 
     return best_tour, best_tour_length
 
+#fuction pour la visualisation du maps 
+def visualize_graph(G):
+    m = folium.Map(location=[20.0, -10.0], zoom_start=6)
+    for u, v, data in G.edges(data=True):
+        # Ajouter la ligne entre les villes avec une flèche
+        folium.PolyLine(
+            locations=[(G.nodes[u]['pos'][0], G.nodes[u]['pos'][1]), (G.nodes[v]['pos'][0], G.nodes[v]['pos'][1])],
+            color='blue', weight=1, opacity=0.5, dash_array='5, 5').add_to(m)
+
+        # Ajouter les marqueurs pour chaque ville
+        folium.Marker(location=(G.nodes[u]['pos'][0], G.nodes[u]['pos'][1]), popup=u,
+                      icon=folium.Icon(color='green', icon='circle', prefix='fa')).add_to(m)
+        folium.Marker(location=(G.nodes[v]['pos'][0], G.nodes[v]['pos'][1]), popup=v,
+                      icon=folium.Icon(color='green', icon='circle', prefix='fa')).add_to(m)
+
+        # Calculer la position du label de distance
+        label_lat = (G.nodes[u]['pos'][0] + G.nodes[v]['pos'][0]) / 2
+        label_lon = (G.nodes[u]['pos'][1] + G.nodes[v]['pos'][1]) / 2
+
+        # Ajouter le label de distance
+        folium.Marker(location=(label_lat, label_lon), icon=folium.DivIcon(
+            html=f'<div style="font-size: 8pt;">{round(data["weight"], 2)} km</div>')).add_to(m)
+
+    # Ajuster le zoom et la position de la carte
+    m.fit_bounds(m.get_bounds())
+
+    return m
+
+
+    
 @app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/upload')
+def uploadfile():
+    return render_template('upload.html')
+
+
+@app.route('/uploadfile', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "Aucun fichier trouvé !"
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        return "Nom de fichier vide !"
+
+    if file:
+        filename = file.filename
+        file.save(filename)
+        return "Fichier téléchargé avec succès !"
+
+
+@app.route('/ant_colony')
 def run_ant_colony_optimization():
     G=upload()
     num_ants = 10
@@ -90,32 +175,35 @@ def run_ant_colony_optimization():
         "best_tour": best_tour,
         "best_tour_length": best_tour_length
     }
+    # Affichage graphique du graphe et de la solution trouvée
+    plt.figure(figsize=(10, 6))
 
-    return jsonify(results)
+    # Affichage du graphe
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, pos, with_labels=True, node_size=200, node_color='skyblue')
+    plt.title('Graphe des villes en Mauritanie')
 
-@app.route('/upload')
-def upload():
-    capitales_df = pd.read_excel('Cordonnees_GPS.xlsx', sheet_name='Capitales_Wilaya')
-    # mougataas_df = pd.read_excel('Cordonnees_GPS.xlsx', sheet_name='Mougataa')
-
-    G = nx.Graph()
+    # Affichage de la solution trouvée
+    best_tour_edges = [(best_tour[i], best_tour[i + 1]) for i in range(len(best_tour) - 1)]
+    nx.draw_networkx_edges(G, pos, edgelist=best_tour_edges, edge_color='red', width=2)
     
-    for _, row in capitales_df.iterrows():
-        ville, latitude, longitude = row
-        G.add_node(ville, pos=(latitude, longitude))
+        # Save plot to a buffer
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
 
-    # for _, row in mougataas_df.iterrows():
-    #     ville, wilaya, latitude, longitude = row
-    #     G.add_node(ville, pos=(latitude, longitude))
+    # Encode plot to base64
+    plot_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
-    for u in G.nodes():
-        for v in G.nodes():
-            if u != v and not G.has_edge(u, v):
-                coords_u = G.nodes[u]['pos']
-                coords_v = G.nodes[v]['pos']
-                distance = geopy.distance.geodesic(coords_u, coords_v).km      
-                G.add_edge(u, v, weight=distance)
-    return G
+    plt.close()
+
+    return render_template('graphe.html', plot=plot_base64)
+
+    
+
+    # return jsonify(results)
+
+
 
 @app.get('/Alg_approx')
 def Alg_approx():
@@ -142,48 +230,104 @@ def Alg_approx():
 
 
 
-@app.get('/map')
-def visualize_graph(G, m):
-    for u, v, data in G.edges(data=True):
-        # Ajouter la ligne entre les villes avec une flèche
-        folium.PolyLine(
-            locations=[(G.nodes[u]['pos'][0], G.nodes[u]['pos'][1]), (G.nodes[v]['pos'][0], G.nodes[v]['pos'][1])],
-            color='blue', weight=1, opacity=0.5, dash_array='5, 5').add_to(m)
 
-        # Ajouter les marqueurs pour chaque ville
-        folium.Marker(location=(G.nodes[u]['pos'][0], G.nodes[u]['pos'][1]), popup=u,
-                      icon=folium.Icon(color='green', icon='circle', prefix='fa')).add_to(m)
-        folium.Marker(location=(G.nodes[v]['pos'][0], G.nodes[v]['pos'][1]), popup=v,
-                      icon=folium.Icon(color='green', icon='circle', prefix='fa')).add_to(m)
-
-        # Calculer la position du label de distance
-        label_lat = (G.nodes[u]['pos'][0] + G.nodes[v]['pos'][0]) / 2
-        label_lon = (G.nodes[u]['pos'][1] + G.nodes[v]['pos'][1]) / 2
-
-        # Ajouter le label de distance
-        folium.Marker(location=(label_lat, label_lon), icon=folium.DivIcon(
-            html=f'<div style="font-size: 8pt;">{round(data["weight"], 2)} km</div>')).add_to(m)
-
-    # Ajuster le zoom et la position de la carte
-    m.fit_bounds(m.get_bounds())
-
-    return m
-
+# @app.get('/Alg_approx')
+# def Alg_approx():
+#     # Retrieve the graph
+#     G = upload()
     
+#     # Calculate the minimum spanning tree
+#     mst = nx.minimum_spanning_tree(G)
 
-    
+#     ville_depart = 'Nouakchott'
+#     ville_arrivee = 'Nouakchott'
+
+#     # Perform DFS traversal on the minimum spanning tree starting from Nouakchott
+#     dfs_edges = list(nx.dfs_edges(mst, source=ville_depart))
+
+#     # Add the last edge to return to Nouakchott
+#     dfs_edges.append((dfs_edges[-1][1], ville_arrivee))
+
+#     # Format the results into JSON format
+#     results = [{"from": edge[0], "to": edge[1]} for edge in dfs_edges]
+
+#     # Matplotlib visualization
+#     plt.figure(figsize=(8, 6))
+
+#     # Draw the graph
+#     pos = nx.spring_layout(G)  # You can choose a layout algorithm that suits your graph
+#     nx.draw(G, pos, with_labels=True, node_size=200, node_color='skyblue')
+#     nx.draw_networkx_edges(G, pos, edgelist=dfs_edges, edge_color='red', width=2)
+
+#     # Save plot to a buffer
+#     buffer = io.BytesIO()
+#     plt.savefig(buffer, format='png')
+#     buffer.seek(0)
+
+#     # Encode plot to base64
+#     plot_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+#     plt.close()
+
+#     return render_template('graphe.html', plot=plot_base64)
+
+#     # Return the results along with the plot
+#     return jsonify({"results": results, "plot": plot_base64})
+
+
 @app.get('/maps')
 def maps():
     G=upload()
-        # Utilisation de la fonction
-    m = folium.Map(location=[20.0, -10.0], zoom_start=6)
-    m = visualize_graph(G, m)
 
-    # Enregistrer la carte dans un fichier HTML
+    m = visualize_graph(G)
+
     m.save('mauritania_map_with_graph8.html')
 
-    # Ouvrir le fichier HTML dans le navigateur par défaut
     webbrowser.open('mauritania_map_with_graph8.html')
 
 
     # return render_template('map.html')
+
+
+
+# @app.get("/test")
+# def test():
+#     G=upload()
+#     num_ants = 10
+#     alpha = 1
+#     beta = 2
+#     evaporation_rate = 0.5
+#     num_iterations = 100
+#     Q = 100
+
+#     # Appliquer l'algorithme de colonie de fourmis
+#     best_tour, best_tour_length = ant_colony_optimization(G, num_ants, alpha, beta, evaporation_rate, num_iterations, Q)
+
+#     results = {
+#         "best_tour": best_tour,
+#         "best_tour_length": best_tour_length
+#     }
+#     # Affichage graphique du graphe et de la solution trouvée
+#     plt.figure(figsize=(10, 6))
+
+#     # Affichage du graphe
+#     pos = nx.get_node_attributes(G, 'pos')
+#     nx.draw(G, pos, with_labels=True, node_size=200, node_color='skyblue')
+#     plt.title('Graphe des villes en Mauritanie')
+
+#     # Affichage de la solution trouvée
+#     best_tour_edges = [(best_tour[i], best_tour[i + 1]) for i in range(len(best_tour) - 1)]
+#     nx.draw_networkx_edges(G, pos, edgelist=best_tour_edges, edge_color='red', width=2)
+
+#     # Save plot to a buffer
+#     buffer = io.BytesIO()
+#     plt.savefig(buffer, format='png')
+#     buffer.seek(0)
+
+#     # Encode plot to base64
+#     plot_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+#     plt.close()
+
+#     return render_template('graphe.html', plot=plot_base64)
+
